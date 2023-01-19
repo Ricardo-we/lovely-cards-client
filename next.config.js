053b483b8bb34dc525ebcee37ecf01a8477affdc
Ricardo-we <totgamer75@gmail.com>
2023-01-19 13:42:05 -0600

    Fixed some bugs

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
+	staticPageGenerationTimeout: 1500,
+	pageExtensions: ['page.tsx',  'page.jsx',]
 };
 
 module.exports = nextConfig;
